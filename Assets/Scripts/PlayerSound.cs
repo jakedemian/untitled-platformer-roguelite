9323db8b8777c7926eb6d243742f46eb01021746
Jake Demian <jakedemian@gmail.com>
2023-02-26 12:04:23 -0500

    added step sounds

--- Assets/Scripts/PlayerSound.cs
+++ Assets/Scripts/PlayerSound.cs
@@ -1,16 +1,35 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerSound : MonoBehaviour {
     public AudioClip jump;
     public AudioClip land;
     public AudioClip step;
+    public float stepLoopTime;
 
     public float jumpPitchVariation;
     public float landPitchVariation;
+    public float stepPitchVariation;
+    private bool isMovingOnGround;
+
+    private PlayerMovement playerMovement;
+    private Rigidbody2D rb;
 
     private void Awake() {
         PlayerEvents.instance.onPlayerJump += OnPlayerJump;
         PlayerEvents.instance.onPlayerLand += OnPlayerLand;
+
+        playerMovement = GetComponent<PlayerMovement>();
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(LoopStepSounds());
+    }
+
+    private void Update() {
+        isMovingOnGround = playerMovement.IsGrounded() && !Mathf.Approximately(rb.velocity.x, 0f);
+    }
+
+    private void OnDestroy() {
+        StopCoroutine(LoopStepSounds());
     }
 
     private void OnPlayerJump() {
@@ -19,5 +38,17 @@ public class PlayerSound : MonoBehaviour {
 
     private void OnPlayerLand() {
         AudioController.instance.PlaySoundVaryPitch(land, landPitchVariation);
+    }
+
+    private IEnumerator LoopStepSounds() {
+        while (true) {
+            if (isMovingOnGround) {
+                AudioController.instance.PlaySoundVaryPitch(step, stepPitchVariation);
+                yield return new WaitForSeconds(stepLoopTime);
+            }
+            else {
+                yield return new WaitForSeconds(stepLoopTime);
+            }
+        }
     }
 }
